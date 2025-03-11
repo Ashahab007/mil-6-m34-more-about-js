10640a66b1d9dd8e40abb34cf0843071f8fae5a1
Ashahab007 <ashahab007@gmail.com>
2025-03-11 13:32:34 +0600

    async await detail with exercise

--- script/2.async await.js
+++ script/2.async await.js
@@ -5,8 +5,8 @@ const loadData = async () => {
   console.log("async/await: আমি দ্বিতীয়");
   let response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
   let data = await response.json();
-  console.log("async/await: ", data);
-  console.log("async/await: আমি তৃতীয়");
+  console.log("async/await: আমি তৃতীয়", data);
+  console.log("async/await: আমি চতুর্থ");
 };
 loadData();
 
@@ -14,8 +14,8 @@ loadData();
 const fetchData = () => {
   fetch("https://jsonplaceholder.typicode.com/todos/1")
     .then((res) => res.json())
-    .then((data) => console.log("fetch", data));
-  console.log("fetch: আমি চতুর্থ");
+    .then((data) => console.log("fetch: আমি প্রথম", data));
+  console.log("fetch: আমি দ্বিতীয় ");
 };
 
 fetchData();
@@ -23,3 +23,19 @@ fetchData();
 /* 
 Note: এখানে async/await এর মাধ্যমে যেকোনো asynchronous operation synchronously করা যায়। যা fetch দিয়ে করা যায় না। see the both result in console.
 */
+
+// Exercise:
+const orderFood = async () => {
+  const food = await cookFood();
+  console.log("Food is ready");
+  console.log(food);
+};
+
+function cookFood() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Food is delicious");
+    }, 3000);
+  });
+}
+orderFood();
